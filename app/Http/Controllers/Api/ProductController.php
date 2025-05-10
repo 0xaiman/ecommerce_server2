0d1c00c8fc9c2ca181a7bf030032a9f7d040b73c
aimandanimsd <aiman.dani@msd.net.my>
2025-05-10 16:31:15 +0800

    feat(Product): Paginate

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -10,7 +10,8 @@ class ProductController extends Controller
 {
     public function index(Request $request)
     {
-        $products = Product::all();
+        $products = Product::paginate(10);
+
         return response()->json($products);
     }
 
