29494cde742e786a8d0357a234c40c669f1366ba
0xaiman <aimandani9797@gmail.com>
2025-05-17 13:50:18 +0800

    update UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,37 +2,57 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use App\Models\User;
-
-use function PHPSTORM_META\type;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        // Admin User
-        User::firstOrCreate(
-            ['email' => 'admin@fruitmart.local'],
+        $users = [
             [
-                'name' => 'Admin User',
-                'password' => Hash::make('password'),
-                'email_verified_at' => now(),
+                'email' => 'dev@mail.com',
+                'name' => 'Developer',
+                'password' => 'password',
+                'type' => 'dev',
+            ],
+            [
+                'email' => 'admin@mail.com',
+                'name' => 'Admin',
+                'password' => 'password',
                 'type' => 'admin',
-            ]
-        );
-
-        // Customer User
-        User::firstOrCreate(
-            ['email' => 'customer@fruitmart.local'],
+            ],
             [
-                'name' => 'Fruit Customer',
-                'password' => Hash::make('password'),
-                'email_verified_at' => now(),
+                'email' => 'manager@mail.com',
+                'name' => 'Manager',
+                'password' => 'password',
+                'type' => 'manager',
+            ],
+            [
+                'email' => 'dataentry@mail.com',
+                'name' => 'Data Entry',
+                'password' => 'password',
+                'type' => 'data_entry',
+            ],
+            [
+                'email' => 'customer@mail.com',
+                'name' => 'Customer',
+                'password' => 'password',
                 'type' => 'client',
-              
-            ]
-        );
+            ],
+        ];
+
+        foreach ($users as $user) {
+            User::firstOrCreate(
+                ['email' => $user['email']],
+                [
+                    'name' => $user['name'],
+                    'password' => Hash::make($user['password']),
+                    'email_verified_at' => now(),
+                    'type' => $user['type'],
+                ]
+            );
+        }
     }
 }
